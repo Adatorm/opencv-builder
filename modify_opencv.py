5d3709c56a5963631317af32d3153c9bc881127f
Guner Acet <kurama632@gmail.com>
2022-08-09 13:24:38 +0300

    opencv modifier updated

--- modify_opencv.py
+++ modify_opencv.py
@@ -1,45 +1,102 @@
 # open specified opencv cmake file and modify target lines
 
+import sys
+import os
 
-from asyncore import write
-
+class LineModifier:
+    def __init__(self) -> None:
+        self.signiture_line_string = "" # unique string to find line number
+        self.actual_line_string = "" # line to modify
+        self.updated_line_string = "" # new line content
+        self.file_path = "" # file path to modify
+        self.offset_from_signiture_line = 0 # target line offset from signiture line
+    
+    def create_linux_remove():
+        linux_remove = LineModifier()
+        linux_remove.file_path = "cmake/OpenCVModule.cmake"
+        linux_remove.signiture_line_string = "  # For dynamic link numbering conventions"
+        linux_remove.actual_line_string = "  if(NOT ANDROID)"
+        linux_remove.updated_line_string = "  if(NOT ANDROID AND NOT UNIX)"
+        linux_remove.offset_from_signiture_line = 1
+        return linux_remove
+    
+    def create_windows_remove():
+        windows_remove = LineModifier()
+        windows_remove.file_path = "cmake/CMakeLists.txt"
+        windows_remove.signiture_line_string = "  # Postfix of DLLs:"
+        windows_remove.actual_line_string = "  ocv_update(OPENCV_DLLVERSION \"${OPENCV_VERSION_MAJOR}${OPENCV_VERSION_MINOR}${OPENCV_VERSION_PATCH}\")"
+        windows_remove.updated_line_string = "  ocv_update(OPENCV_DLLVERSION \"\")"
+        windows_remove.offset_from_signiture_line = 1
+        return windows_remove
 
 class VARS:
-    opencv_cmake_module_path = "opencv/cmake/OpenCVModule.cmake"
-    target_line_sign = "  # For dynamic link numbering conventions"
-    actual_line = "  if(NOT ANDROID)"
-    updated_line = "  if(NOT ANDROID AND LINUX AND WIN32)"
+    linux_remove = LineModifier.create_linux_remove()
+    windows_remove = LineModifier.create_windows_remove()
 
 
 
 # read file
-def read_file_as_lines():
-    with open(VARS.opencv_cmake_module_path) as file:
+def read_file_as_lines(path: str):
+    with open(path) as file:
         lines = file.readlines()
     return lines
 
 
 
 # write to files
-def write_file(lines: str):
-    with open(VARS.opencv_cmake_module_path, "w") as file:
+def write_file(path: str, lines: list):
+    with open(path, "w") as file:
         file.writelines(lines)
 
 
-def modify_file(lines):
+def modify_line_helper(lines: list, line_modifier: LineModifier):
+    
     for i in range(len(lines)):
-        if lines[i].startswith(VARS.target_line_sign):
-            if len(lines) > i+1 and lines[i+1].startswith(VARS.actual_line):
-                lines[i+1] = lines[i+1].replace(VARS.actual_line, VARS.updated_line)
-                print("replaced: " + VARS.actual_line + " -> " + VARS.updated_line)
+        
+        if lines[i].startswith(line_modifier.signiture_line_string):
+            
+            offset = line_modifier.offset_from_signiture_line
+            if len(lines) > i+offset \
+                 and lines[i+offset].startswith(line_modifier.actual_line_string):
+                
+                lines[i+offset] = lines[i+offset].replace(line_modifier.actual_line_string, line_modifier.updated_line_string)
+                print("replaced: " + line_modifier.actual_line_string + " -> " + line_modifier.updated_line_string)
+            elif len(lines) > i+offset \
+                and lines[i+offset].startswith(line_modifier.updated_line_string):
+                
+                print("same string! no replacement!: " + line_modifier.updated_line_string + " -> " + line_modifier.updated_line_string)
+            else:
+                
+                print("error occured! ")
+                print("not replaced!: " + line_modifier.actual_line_string + " -> " + line_modifier.updated_line_string)
+                return
 
     return lines
 
 
+
+def modify_line(line_modifier: LineModifier):
+    actual_path = os.path.join(sys.argv[1], line_modifier.file_path)
+    lines = read_file_as_lines(actual_path)
+    lines = modify_line_helper(lines, line_modifier)
+    write_file(actual_path, lines)
+
+
+def modify_sources():
+    modify_line(VARS.windows_remove)
+    modify_line(VARS.linux_remove)
+
+def print_usage():
+    print("usage: modify_opencv.py <opencv_dir>")
+    print("example:")
+    print("python3 modify_opencv.py .")
+    print("python3 modify_opencv.py opencv")
+
 def program():
-    lines = read_file_as_lines()
-    lines = modify_file(lines)
-    write_file(lines)
+    if len(sys.argv) != 2:
+        print_usage()
+    else:
+        modify_sources()
 
 
 if __name__ == "__main__":
